5a733e604e7181d42feb17d850fcd2c8a7c977eb
Oualid R. <oualid.rhechim@gmail.com>
2025-06-30 13:29:07 +0200

    Created Customer entity and CustomerRepository for BookingService

--- bookingservice/src/main/java/com/rhechim/bookingservice/service/BookingService.java
+++ bookingservice/src/main/java/com/rhechim/bookingservice/service/BookingService.java
@@ -1,11 +1,20 @@
 package com.rhechim.bookingservice.service;
 
+import com.rhechim.bookingservice.repository.CustomerRepository;
 import com.rhechim.bookingservice.request.BookingRequest;
 import com.rhechim.bookingservice.response.BookingResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookingService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public BookingService(final CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     public BookingResponse createBooking(final BookingRequest request) {
         return new BookingResponse().builder().build();
