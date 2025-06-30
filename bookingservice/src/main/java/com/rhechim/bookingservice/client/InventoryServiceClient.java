ece465626ae2d4d6e254c9103ef586ef827a7dcc
Oualid R. <oualid.rhechim@gmail.com>
2025-06-30 13:55:12 +0200

    created VenueResponse and Inventory Response for InventoryServiceClient

--- bookingservice/src/main/java/com/rhechim/bookingservice/client/InventoryServiceClient.java
+++ bookingservice/src/main/java/com/rhechim/bookingservice/client/InventoryServiceClient.java
@@ -1,4 +1,21 @@
 package com.rhechim.bookingservice.client;
 
+
+
+import com.rhechim.bookingservice.response.InventoryResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class InventoryServiceClient {
+
+    @Value("${inventory.service.url}")
+    private String inventoryServiceUrl;
+
+    public InventoryResponse getInventory(final Long eventId) {
+        final RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate.getForObject(inventoryServiceUrl + "/events/" + eventId, InventoryResponse.class);
+    }
 }
--- bookingservice/src/main/java/com/rhechim/bookingservice/service/BookingService.java
+++ bookingservice/src/main/java/com/rhechim/bookingservice/service/BookingService.java
@@ -1,5 +1,6 @@
 package com.rhechim.bookingservice.service;
 
+import com.rhechim.bookingservice.client.InventoryServiceClient;
 import com.rhechim.bookingservice.entity.Customer;
 import com.rhechim.bookingservice.repository.CustomerRepository;
 import com.rhechim.bookingservice.request.BookingRequest;
@@ -11,10 +12,11 @@ import org.springframework.stereotype.Service;
 public class BookingService {
 
     private final CustomerRepository customerRepository;
-
+    private final InventoryServiceClient inventoryServiceClient;
     @Autowired
-    public BookingService(final CustomerRepository customerRepository) {
+    public BookingService(final CustomerRepository customerRepository, final InventoryServiceClient inventoryServiceClient) {
         this.customerRepository = customerRepository;
+        this.inventoryServiceClient = inventoryServiceClient;
     }
 
     public BookingResponse createBooking(final BookingRequest request) {
